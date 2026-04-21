da760b6742547ab317dde5b302f0e8b6d56943ff
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-21 15:34:46 -0300

    feat: add ButtonUser component with IconUser

---
+++ components/buttons/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <Button>
+    <IconUser />
+  </Button>
+</template>
+
+<script setup>
+import Button from '../Button.vue';
+import IconUser from '../icons/IUser.vue';
+
+defineOptions({
+  name: 'ButtonUser',
+});
+
+</script>
+
+<style>
+</style>
---
+++ components/icons/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+  >
+    <circle cx="12" cy="8" r="4" />
+    <path d="M4 20c0-4 4-6 8-6s8 2 8 6" />
+  </svg>
+</template>
+
+<script setup>
+
+defineOptions({
+    name: 'IconUser',
+});
+
+</script>
