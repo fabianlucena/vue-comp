bc2f1458c2dc46b4ca9adb10779871b6e265d5d8
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-21 15:34:29 -0300

    feat: add ButtonMenu and IconMenu components

---
+++ components/buttons/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <Button>
+    <IconMenu />
+  </Button>
+</template>
+
+<script setup>
+import Button from '../Button.vue';
+import IconMenu from '../icons/Menu.vue';
+
+defineOptions({
+  name: 'ButtonMenu',
+});
+
+</script>
+
+<style>
+</style>
---
+++ components/icons/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+  >
+    <rect x="3" y="5" width="18" height="2" rx="1" />
+    <rect x="3" y="11" width="18" height="2" rx="1" />
+    <rect x="3" y="17" width="18" height="2" rx="1" />
+  </svg>
+</template>
+
+<script setup>
+
+defineOptions({
+    name: 'IconMenu',
+});
+
+</script>
